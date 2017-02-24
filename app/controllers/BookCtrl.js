11ab22c2002e71260c1a9d5c2bf3affeb943a116
“Aaron <barfootaaron@yahoo.com>
2017-02-24 13:43:30 -0600

    MVP for Part 1, 2, & 3

--- app/controllers/BookCtrl.js
+++ app/controllers/BookCtrl.js
@@ -1,1 +1,9 @@
 "use strict";
+
+app.controller('BookCtrl', function($scope, GuideFactory) {
+
+	GuideFactory.loadGuides()
+	.then(function(bookCollection) {
+		$scope.books = bookCollection;
+	});
+});
--- app/factories/GuideFactory.js
+++ app/factories/GuideFactory.js
@@ -1,1 +1,18 @@
-"use strict";
+'use strict';
+
+app.factory("GuideFactory", function ($q, $http) {
+
+  let loadGuides = (function() {
+    return $q (function (resolve, reject) {
+      $http.get('./guides.json')
+      .then( function (recievedData) {
+      	var x = recievedData.data.guides;
+      	resolve(x);
+      }).catch(function (error) {
+        	reject(error);
+      });
+    });
+  });
+
+  return {loadGuides};
+});
